21b9ed92c2530437d89f05feea50a2c7a5953ce9
designbynuff <info@designbynuff.com>
2024-01-18 16:40:54 +0100

    Modal Fixed, Name Field Temporarily Disabled.
    As above (so below).

--- public/app.js
+++ public/app.js
@@ -14,59 +14,54 @@ window.addEventListener('load', () => {
     //PORTAL SUBMISSION STUFF (MAYBE MOVE TO SEPARATE FUNCTIONS?)
 
     // When input field has something in it, add right arrow arrow to button
-    nameField.addEventListener('input', () => {
-        if (nameField.value) {
-            nameButton.innerHTML = 'Ready →';
-        } else {
-            nameButton.innerHTML = 'Ready';
-        }
-    });
+    // nameField.addEventListener('input', () => {
+    //     if (nameField.value) {
+    //         nameButton.innerHTML = 'Ready →';
+    //     } else {
+    //         nameButton.innerHTML = 'Ready';
+    //     }
+    // });
 
     // Execute a function when the user presses a key on the keyboard
-    nameField.addEventListener("keypress", function (event) {
-        // If the user presses the "Enter" key on the keyboard
-        if (event.key === "Enter") {
-            // Cancel the default action, if needed
-            event.preventDefault();
-            // Trigger the button element with a click
-            nameButton.click();
-        }
-    });
+    // nameField.addEventListener("keypress", function (event) {
+    //     // If the user presses the "Enter" key on the keyboard
+    //     if (event.key === "Enter") {
+    //         // Cancel the default action, if needed
+    //         event.preventDefault();
+    //         // Trigger the button element with a click
+    //         nameButton.click();
+    //     }
+    // });
 
     // When button is clicked, get name and send to server
-    nameButton.addEventListener('click', () => {
-        // Get name from input field
-        userName = nameField.value;
-        console.log(userName);
+    // nameButton.addEventListener('click', () => {
+    //     // Get name from input field
+    //     userName = nameField.value;
+    //     console.log(userName);
 
-        // Send name to server
-        socket.emit('name', name);
-    });
+    //     // Send name to server
+    //     socket.emit('name', name);
+    // });
 
     // Get the modal
-    // var modal = document.getElementById("myModal");
+    var modal = document.getElementById("my-modal");
 
-    // // Get the button that opens the modal
-    // var modalBtn = document.getElementById("info");
+    // Get the button that opens the modal
+    var modalBtn = document.getElementById("modal-btn");
 
-    // // Get the <span> element that closes the modal
-    // var span = document.getElementsByClassName("close")[0];
+    // Get the x that closes the modal
+    var closeBtn = document.getElementById("close-btn");
 
-    // // When the user clicks on the button, open the modal
-    // modalBtn.onclick = function () {
-    //     modal.style.display = "block";
-    // }
+    // When the user clicks on the button, open the modal
+    modalBtn.addEventListener('click', () => {
+        console.log('opening modal');
+        modal.style.display = "block";
+    });
 
-    // // When the user clicks on <span> (x), close the modal
-    // span.onclick = function () {
-    //     modal.style.display = "none";
-    // }
-
-    // // When the user clicks anywhere outside of the modal, close it
-    // window.onclick = function (event) {
-    //     if (event.target == modal) {
-    //         modal.style.display = "none";
-    //     }
-    // }
+    // When the user clicks on (x), close the modal
+    closeBtn.addEventListener('click', () => {
+        console.log('closing modal');
+        modal.style.display = "none";
+    });
 
 });
